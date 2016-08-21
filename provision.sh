8be69907171c8e7d4d5e258a4d62342c79b4dfa7
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-08-21 16:19:20 -0500

    Adding provisioner

--- provision.sh
+++ provision.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+DOCKER_VERSION=1.7.1~trusty
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+sudo bash -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
+
+sudo apt-get update
+sudo apt-get install -y docker-engine=${DOCKER_VERSION}
+
+sudo rm -rf /var/lib/apt/lists/*
